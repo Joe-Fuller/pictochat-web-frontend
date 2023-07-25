da2ac790d9d075b77742b88dc59cd1d265f3e48b
Joe <joefuller042@gmail.com>
2023-07-25 18:06:39 +0100

    move message text to the right

--- components/message.js
+++ components/message.js
@@ -1,10 +1,13 @@
 export default function Message({ message }) {
   return (
-    <li className="bg-white border rounded-lg p-2 mb-2">
-      <span className="text-gray-800 text-sm font-semibold">
+    <li className="relative bg-white border-red-600 border-2 rounded-lg p-2 mb-2 text-black">
+      <div className="absolute top-0 left-0 pl-1 bg-red-400 text-sm font-semibold rounded-br-md rounded-tl border-2 border-red-600">
         {message.nickname}
-      </span>
-      <span dangerouslySetInnerHTML={{ __html: message.message }}></span>
+      </div>
+      <div
+        className="pl-8"
+        dangerouslySetInnerHTML={{ __html: message.message }}
+      ></div>
     </li>
   );
 }
