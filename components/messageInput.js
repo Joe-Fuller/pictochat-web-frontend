41551768be1f5a690448c77b0af1f4af98f227e8
Joe <joefuller042@gmail.com>
2023-07-25 15:17:27 +0100

    add messageInput component

---
+++ components/messageInput.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import io from "socket.io-client";
+import DrawingArea from "./drawingArea";
+
+const serverURL = "http://localhost:3000";
+
+export default function MessageInput({ nickname }) {
+  const [inputValue, setInputValue] = useState("");
+  const socket = io(serverURL);
+
+  const handleMessageSend = () => {
+    socket.emit("message", `${nickname}: ${inputValue}`);
+    setInputValue("");
+  };
+
+  return (
+    <div>
+      <input
+        className="text-black"
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={handleMessageSend}>Send</button>
+      <DrawingArea></DrawingArea>
+    </div>
+  );
+}
