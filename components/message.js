ce725331b794b52a95a514748ed76620739ab29c
Joe <joefuller042@gmail.com>
2023-07-26 12:00:15 +0100

    increase nickname size

--- components/message.js
+++ components/message.js
@@ -4,7 +4,7 @@ export default function Message({ message, colours }) {
       className={`relative bg-white ${colours.borderColour} border-2 rounded-lg p-2 mb-2 text-black z-10`}
     >
       <div
-        className={`absolute top-0 left-0 px-1 ${colours.bgColour} text-sm font-semibold rounded-br-md rounded-tl border-r-2 border-b-2 ${colours.borderColour} z-0`}
+        className={`absolute top-0 left-0 px-1 ${colours.bgColour} text-xl font-semibold rounded-br-md rounded-tl border-r-2 border-b-2 ${colours.borderColour} z-0`}
       >
         {message.nickname}
       </div>
