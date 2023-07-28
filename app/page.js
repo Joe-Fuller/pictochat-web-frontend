d55094fe6c68894de02fb054f7850a543f264b58
Joe <joefuller042@gmail.com>
2023-07-28 17:20:01 +0100

    testing testing testing

--- app/page.js
+++ app/page.js
@@ -13,14 +13,19 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen mobile:min-h-full">
-      <h1 className="text-2xl font-bold py-4">PictoChat</h1>
-      <ChatHistory />
-      <div className="flex-grow" />
-      <MessageInput
-        nickname={nickname}
-        className="bg-white p-4 overflow-hidden mobile:overflow-y-auto"
-      />
+    <div className="flex flex-col min-h-screen">
+      <h1 className="text-2xl font-bold py-4 text-center fixed top-0 w-full">
+        PictoChat
+      </h1>
+      <div className="flex flex-col flex-grow justify-center items-center">
+        <ChatHistory />
+      </div>
+      <div className="flex-shrink-0 flex justify-center">
+        <MessageInput
+          nickname={nickname}
+          className="bg-white p-4 overflow-hidden fixed bottom-0 left-0 right-0 w-full mobile:max-h-[70vh] mobile:overflow-y-auto"
+        />
+      </div>
     </div>
   );
 }
